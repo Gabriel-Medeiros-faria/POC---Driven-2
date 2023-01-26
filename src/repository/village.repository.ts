cff54040389080f5a99df1fc5ab54dc0307118a4
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2023-01-26 15:24:44 -0300

    final version 2.0

--- src/repository/village.repository.ts
+++ src/repository/village.repository.ts
@@ -5,9 +5,3 @@ import { village, villageEntity } from "../protocols/village.protocol.js";
 export async function findAllRep() : Promise<QueryResult<villageEntity>> {
     return prisma.village.findMany()
  }
-
-export async function createRep(body) : Promise<QueryResult<villageEntity>> {
-    return prisma.village.create({
-        data: body
-    })
-} 
